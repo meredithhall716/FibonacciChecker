f9e6f3c43ff037e5d0c96f974917a6a794314020
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-05-25 16:28:42 -0400

    Fibonacci

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,13 @@
 const fibonacci = (num) => {
-    
+    let fibArr = [0, 1];
+    let answer;
+
+    for (let i = 2; i <= num; i++) {
+        fibArr[i] = fibArr[i-1] + fibArr[i - 2]
+    }
+
+    answer = fibArr[num]
+    return answer
 }
 
 module.exports = {fibonacci}
